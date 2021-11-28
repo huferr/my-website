ad6d81b6084a61ba46ac83a190f28992d5a763d0
Hugo Ferreira <hugoferreirait@gmail.com>
2021-11-28 03:21:17 -0300

    feat: styling scrollbar

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -1,4 +1,4 @@
-import {createGlobalStyle} from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
     html,
@@ -7,4 +7,18 @@ export const GlobalStyles = createGlobalStyle`
     margin: 0;
     font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
         Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-}`;
+    }
+    body::-webkit-scrollbar {
+    background-color: #201E1E;
+    width: 5px;
+    }
+ 
+    body::-webkit-scrollbar-track {
+        background-color: #201E1E;
+    }
+ 
+    body::-webkit-scrollbar-thumb {
+    background: linear-gradient(50deg, #53cadf, #fff385, #ff66b0);
+    border-radius: 20px;
+    }
+`;
