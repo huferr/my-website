0628cdba0a593985543e6588f8c8d05f73c633ea
Hugo Ferreira <hugoferreirait@gmail.com>
2021-11-28 03:20:41 -0300

    feat: project data

---
+++ src/data/projects.tsx
@@ -0,0 +1,26 @@
+import Mac from '../assets/images/mac.png';
+
+export const Projects = [
+  {
+    id: 1,
+    name: 'Math Viewer App',
+    image: Mac,
+    languages: 'React Native, Typescript, Styled-Components',
+    platform: 'Android & Ios',
+  },
+  {
+    id: 2,
+    name: 'Capitual App',
+    image: Mac,
+    languages: 'React Native, Typescript, Styled-Components',
+    platform: 'Android & Ios',
+    content: 'Working on the mobile team of the world’s first hybrid bank app.',
+  },
+  {
+    id: 3,
+    name: 'SpaceX Website Emulation',
+    image: Mac,
+    languages: 'HTML, CSS, Javascript',
+    platform: 'Web',
+  },
+];
