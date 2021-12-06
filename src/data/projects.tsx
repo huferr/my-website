0f2562b95c02373f4003368c269202b6297a72f4
Hugo Ferreira <hugoferreirait@gmail.com>
2021-12-06 12:10:47 -0300

    chore: adding project links

--- src/data/projects.tsx
+++ src/data/projects.tsx
@@ -7,6 +7,7 @@ export const Projects = [
     image: Mac,
     languages: 'React Native, Typescript, Styled-Components',
     platform: 'Android & Ios',
+    link: 'https://github.com/huferr/math_viewer',
   },
   {
     id: 2,
@@ -15,6 +16,7 @@ export const Projects = [
     languages: 'React Native, Typescript, Styled-Components',
     platform: 'Android & Ios',
     content: 'Working on the mobile team of the world’s first hybrid bank app.',
+    link: 'https://apps.apple.com/app/id1458793819',
   },
   {
     id: 3,
@@ -22,5 +24,6 @@ export const Projects = [
     image: Mac,
     languages: 'HTML, CSS, Javascript',
     platform: 'Web',
+    link: 'https://github.com/huferr/spacex-rockets-shop',
   },
 ];
