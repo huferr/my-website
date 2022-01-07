1dbd0954bcb1b88a9ea577b488f7c4766b97e7c5
Hugo Ferreira <hugoferreirait@gmail.com>
2022-01-07 00:13:01 -0300

    chore: typography

---
+++ src/styles/typography.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+// base headings, subtitles and paragraphs
+export const Heading = styled.h1``;
+
+export const Subtitle = styled.h2``;
+
+export const Paragraph = styled.p``;
